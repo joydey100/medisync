efd6ecebcdb47bc1f7bbf22f98771bfbcbc9f3e6
Joy <joydey.bd.2002@gmail.com>
2025-05-08 22:47:00 +0600

    change secret

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -9,7 +9,7 @@ import Stripe from "stripe";
 import dotenv from "dotenv";
 dotenv.config();
 
-const stripe = Stripe(process.env.STRIPE_SECRET);
+const stripe = Stripe(process.env.STRIPE_API_SECRET);
 
 //  API to register user
 export const registerUser = async (req, res) => {
